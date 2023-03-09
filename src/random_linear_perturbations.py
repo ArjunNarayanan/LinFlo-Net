6c7250f9ae19ffd5eb23906b5527fa573df3a522
Arjun Narayanan <arjun.narayanan@berkeley.edu>
2023-03-09 14:12:48 -0800

    adding all config files

--- src/random_linear_perturbations.py
+++ src/random_linear_perturbations.py
@@ -1,20 +1,24 @@
 from src.linear_transform import LinearTransformer
 import torch
 
+device = torch.device('cuda:' + str(0) if torch.cuda.is_available() else 'cpu')
 
 def get_random_scaling(batch_size, magnitude):
     perturbation = magnitude * (2 * torch.rand([batch_size, 3]) - 1)
     perturbation = 1 - perturbation
+    perturbation = perturbation.to(device)
     return perturbation
 
 
 def get_random_translation(batch_size, magnitude):
     perturbation = magnitude * (2 * torch.rand([batch_size, 3]) - 1)
+    perturbation = perturbation.to(device)
     return perturbation
 
 
 def get_random_rotation(batch_size, magnitude):
     perturbation = 2 * torch.pi * magnitude * (2 * torch.rand([batch_size, 3]) - 1)
+    perturbation = perturbation.to(device)
     return perturbation
 
 
