6ea44de44cbcbcde58570b87e4844eee12b8e669
Arjun <arjun.narayanan91@gmail.com>
2023-09-14 12:17:03 -0700

    modified plot loss history to accept CL args

--- utilities/plot_loss_history.py
+++ utilities/plot_loss_history.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import argparse
 
 
 def plot_loss(train_loss, validation_loss, loss_name, filename=None):
@@ -28,20 +29,23 @@ def plot_all_losses(train_df, validation_df, output_dir):
         plot_loss(train_loss, validation_loss, loss_name, outfile)
 
 
-root_dir = "output/WholeHeartData/trained_models/ct/combined/flow/model-3"
-validation_loss_file = os.path.join(root_dir, "validation_loss.csv")
-train_loss_file = os.path.join(root_dir, "train_loss.csv")
-assert os.path.isdir(root_dir)
-assert os.path.isfile(validation_loss_file)
-assert os.path.isfile(train_loss_file)
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description="plot loss history from training logs")
+    parser.add_argument("-input", help="input folder", required=True)
+    args = parser.parse_args()
 
-output_dir = os.path.join(root_dir, "loss_plots")
-if not os.path.isdir(output_dir):
-    os.makedirs(output_dir)
+    root_dir = args.input
+    validation_loss_file = os.path.join(root_dir, "validation_loss.csv")
+    train_loss_file = os.path.join(root_dir, "train_loss.csv")
+    assert os.path.isdir(root_dir)
+    assert os.path.isfile(validation_loss_file)
+    assert os.path.isfile(train_loss_file)
 
+    output_dir = os.path.join(root_dir, "loss_plots")
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
 
+    train_df = pd.read_csv(train_loss_file)
+    validation_df = pd.read_csv(validation_loss_file)
 
-train_df = pd.read_csv(train_loss_file)
-validation_df = pd.read_csv(validation_loss_file)
-
-plot_all_losses(train_df, validation_df, output_dir)
+    plot_all_losses(train_df, validation_df, output_dir)
